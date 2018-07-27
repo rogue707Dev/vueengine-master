d065962a14aee02b9aaf6f67777a6a2569c1dd5c
rogue707dev <rogue901126@protonmail.com>
2018-07-27 14:17:38 +0100

    Catching up with engine changes

--- assets/images/Font/Definition/NumberFontDefinition.c
+++ assets/images/Font/Definition/NumberFontDefinition.c
@@ -58,6 +58,9 @@ FontROMDef NUMBER_FONT =
 	// character number at which the font starts, allows you to skip the control characters for example
 	46,
 
+	// number of characters in this font
+	12,
+
 	// size of a single character (in chars) ({width, height})
 	{1, 1},
 
